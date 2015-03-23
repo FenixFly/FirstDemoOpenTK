371b8f8ecd3b53b2f92d8c454a58d26d3ec35334
FenixFly <eugene.unn@gmail.com>
2015-03-23 16:49:17 +0300

    Adding and coloring GLcontrol

--- Helicopter/Form1.Designer.cs
+++ Helicopter/Form1.Designer.cs
@@ -28,13 +28,30 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.glControl1 = new OpenTK.GLControl();
             this.SuspendLayout();
+            // 
+            // glControl1
+            // 
+            this.glControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.glControl1.BackColor = System.Drawing.Color.Black;
+            this.glControl1.Location = new System.Drawing.Point(12, 12);
+            this.glControl1.Name = "glControl1";
+            this.glControl1.Size = new System.Drawing.Size(560, 537);
+            this.glControl1.TabIndex = 0;
+            this.glControl1.VSync = false;
+            this.glControl1.Load += new System.EventHandler(this.glControl1_Load);
+            this.glControl1.Paint += new System.Windows.Forms.PaintEventHandler(this.glControl1_Paint);
+            this.glControl1.Resize += new System.EventHandler(this.glControl1_Resize);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(584, 561);
+            this.Controls.Add(this.glControl1);
             this.Name = "Form1";
             this.Text = "Helicopter";
             this.ResumeLayout(false);
@@ -42,6 +59,8 @@
         }
 
         #endregion
+
+        private OpenTK.GLControl glControl1;
     }
 }
 
--- Helicopter/Form1.cs
+++ Helicopter/Form1.cs
@@ -6,14 +6,59 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using OpenTK.Graphics.OpenGL;
+using OpenTK;
 
 namespace Helicopter
 {
     public partial class Form1 : Form
     {
+        bool loaded = false;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void glControl1_Load(object sender, EventArgs e)
+        {
+            loaded = true;
+            GL.ClearColor(Color.SkyBlue);
+            SetupViewport();
+        }
+
+        private void SetupViewport()
+        {
+            int w = glControl1.Width;
+            int h = glControl1.Height;
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadIdentity();
+            GL.Ortho(0, w, 0, h, -1, 1); // Bottom-left corner pixel has coordinate (0, 0)
+            GL.Viewport(0, 0, w, h); // Use all of the glControl painting area
+        }
+
+        private void glControl1_Resize(object sender, EventArgs e)
+        {
+            SetupViewport();
+            glControl1.Invalidate();
+        }
+
+        private void glControl1_Paint(object sender, PaintEventArgs e)
+        {
+            Render();
+        }
+
+        private void Render()
+        {
+            if (!loaded)
+                return;
+
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadIdentity();
+
+            glControl1.SwapBuffers();
         }
     }
 }
